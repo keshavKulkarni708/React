f2e3863639c4c053da2c1ba051e2fee851c4cc20
Keshav <keshav.kulkarni708>
2023-11-30 11:32:20 +0530

    Creating todotable component

--- react-todos/src/App.js
+++ react-todos/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import TodoRowItem from "./components/TodoRowItem";
+import TodotoTable from "./components/TodoTable";
 
 function App() {
   const todos = [
@@ -13,34 +13,7 @@ function App() {
       <div className="card">
         <div className="card-header">Your Todo's</div>
         <div className="card-body">
-          <table className="table table-hover">
-            <thead>
-              <tr>
-                <th scope="col">#</th>
-                <th scope="col">Description</th>
-                <th scope="col">Assigned</th>
-              </tr>
-            </thead>
-            <tbody>
-              <TodoRowItem
-                rowNumber={todos[0].rowNumber}
-                rowDescription={todos[0].rowDescription}
-                rowAssigned={todos[0].rowAssigned}
-              />
-
-              <TodoRowItem
-                rowNumber={todos[1].rowNumber}
-                rowDescription={todos[1].rowDescription}
-                rowAssigned={todos[1].rowAssigned}
-              />
-
-              <TodoRowItem
-                rowNumber={todos[2].rowNumber}
-                rowDescription={todos[2].rowDescription}
-                rowAssigned={todos[2].rowAssigned}
-              />
-            </tbody>
-          </table>
+          <TodotoTable todos={todos} />
         </div>
       </div>
     </div>
